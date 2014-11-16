ebf79413e17a7c782ebba520f1fb1b85f15cdb87
Graham Heath <Graham.Heath@rga.com>
2014-11-16 13:04:55 -0600

    remove /platforms from repo

--- platforms/ios/www/js/faCamera.js
+++
@@ -1,7 +0,0 @@
-var FaCamera = {
-getPicture: function(success, failure){
-  console.log("getPicture");
-		cordova.exec(success, failure, "FaCamera", "openCamera", []);
-}
-};
-module.exports = FaCamera;
